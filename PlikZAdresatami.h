0de0d8a3d6e74037dd40d8d965cb644c615a4525
Karol Sołek <karol.solek.programista@gmail.com>
2019-01-20 22:22:33 +0100

    Add classes: Adresat and PlikZAdresatami

---
+++ PlikZAdresatami.h
@@ -0,0 +1,28 @@
+#ifndef PLIKZADRESATAMI_H
+#define PLIKZADRESATAMI_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdlib>
+
+#include "Adresat.h"
+#include "MetodyPomocnicze.h"
+
+using namespace std;
+
+class PlikZAdresatami {
+    const string nazwaPlikuZAdresatami;
+    fstream plikTekstowy;
+
+    //bool czyPlikJestPusty();
+    //string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    //Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
+public:
+    //void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    //vector <Uzytkownik> wczytajUzytkownikowZPliku();
+    //void zapiszWszystkichUzytkownikowDoPliku(vector <Uzytkownik> uzytkownicy);
+    PlikZAdresatami(string nazwa) : nazwaPlikuZAdresatami(nazwa) {};
+};
+
+#endif
